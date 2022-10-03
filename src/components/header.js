7d3425ec3c8f06be581b751ea3a68e6e302e200c
aleja <92136921+mariather@users.noreply.github.com>
2022-10-02 21:36:20 -0700

    add header.js component functionality

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const header = () => {
+    return (
+        <header>
+            <div className="j-s">
+                <span> Alejandra Lopez </span>
+            </div>
+            <div className="j-e">
+                <a href="#landing">Top</a>
+                <a href="#project">Projects</a>
+                <a href="#about">About</a>
+                <a href="#sandbox">Sandbox</a>
+            </div>
+        </header>
+    )
+}
+
+export default header
