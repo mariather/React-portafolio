259dfd8534a81bb5bbe701b4644de103a33496cf
aleja <92136921+mariather@users.noreply.github.com>
2022-10-02 21:42:25 -0700

    create rest of components and connect them to the app

--- src/app.js
+++ src/app.js
@@ -1,35 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header'
+import Hero from './components/hero'
+import Projects from './components/projects'
+import About from './components/about'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      <header>
-        <div className="j-s">
-          <span>Alejandra Lopez</span>
-        </div>
-        <div className="j-e">
-          <span>Alejandra Lopez</span>
-        </div>
-      </header>
-     
-        <p>projects</p>
-      
-      </header>
+      <Header />
+      <main>
+        <Hero />
+        <Projects />
+        <About />
+      </main>
     </div>
-    
   );
 }
+
+export default App;
