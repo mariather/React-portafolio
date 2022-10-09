ade429b4072ee3ef169de9febb3dfcb39d7c7dcc
aleja <92136921+mariather@users.noreply.github.com>
2022-10-09 00:26:26 -0700

    add extra content div with Github and LinkedIn info

--- src/components/header.js
+++ src/components/header.js
@@ -6,6 +6,11 @@ const header = () => {
             <div className="j-s">
                 <span> Alejandra Lopez </span>
             </div>
+            <div id="extra-content">
+                <a href="https://github.com/mariather" target="_blank" rel="noreferrer" >GitHub</a>
+                <a href="https://www.linkedin.com/in/mariather/" target="_blank" rel="noreferrer" >LinkedIn</a>
+               
+            </div>
             <div className="j-e">
                 <a href="#landing">Home</a>
                 <a href="#project">Project List</a>
